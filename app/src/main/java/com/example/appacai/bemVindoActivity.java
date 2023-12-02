1ed03230fc71ec3f671b196431f80e8acc393bdf
Joel Lucas <joellucaspinheiro8@gmail.com>
2023-12-02 09:40:55 -0300

    Bem vindo! Cadastre - se!

---
+++ app/src/main/java/com/example/appacai/bemVindoActivity.java
@@ -0,0 +1,14 @@
+package com.example.appacai;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class bemVindoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bem_vindo);
+    }
+}
